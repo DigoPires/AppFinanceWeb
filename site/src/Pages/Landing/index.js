6cbbfe461f1145f1af6f3fb4420a693b7065bd18
JoaoGstvo <joaogustavocarmomoraes@gmail.com>
2026-01-29 22:20:57 -0300

    First Commit - Basic React Structure

---
+++ site/src/Pages/Landing/index.js
@@ -0,0 +1,12 @@
+import "./index.scss";
+
+function Landing() {
+  return (
+    <div className="landing-container">
+      <h1>Welcome to AppFinance</h1>
+      <p>Your personal finance management app.</p>
+    </div>
+  );
+}
+
+export default Landing;
